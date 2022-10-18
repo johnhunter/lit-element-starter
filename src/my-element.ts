a06b2d0cbd24f67422fe479d3e5030dc1ee211fa
John Hunter <john@johnhunter.info>
2022-10-18 21:47:54 +0100

    Initial commit

---
+++ src/my-element.ts
@@ -0,0 +1,70 @@
+import {LitElement, html, css} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+
+/**
+ * An example element.
+ *
+ * @fires count-changed - Indicates when the count changes
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
+@customElement('my-element')
+export class MyElement extends LitElement {
+  static override styles = css`
+    :host {
+      display: block;
+      border: solid 1px gray;
+      padding: 16px;
+      max-width: 800px;
+      font-family: monospace;
+    }
+    button {
+      border: 1px solid #ccc;
+      border-radius: 0.5rem;
+      padding: 0.5rem 1rem;
+      font-family: inherit;
+    }
+    b {
+      color: green;
+    }
+  `;
+
+  /**
+   * The name to say "Hello" to.
+   */
+  @property()
+  override name = 'World';
+
+  /**
+   * The number of times the button has been clicked.
+   */
+  @property({type: Number})
+  count = 0;
+
+  override render() {
+    return html`
+      <h1>${this.sayHello(this.name)}!</h1>
+      <button @click=${this.onClick}>Click Count: <b>${this.count}</b></button>
+      <slot></slot>
+    `;
+  }
+
+  private onClick() {
+    this.count++;
+    this.dispatchEvent(new CustomEvent('count-changed'));
+  }
+
+  /**
+   * Formats a greeting
+   * @param name The name to say "Hello" to
+   */
+  sayHello(name: string): string {
+    return `Hello, ${name}`;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'my-element': MyElement;
+  }
+}
